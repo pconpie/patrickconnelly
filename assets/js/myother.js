d7f7e13d0dc4704c3f175a3bcec2113beecef33b
Patrick Connelly <patrickconnelly95@gmail.com>
2018-05-14 14:32:09 -0500

    fix menu retracting issue

--- assets/js/myother.js
+++ assets/js/myother.js
@@ -8,3 +8,9 @@ $('#nav-menu').on('click', function() {
     $('#about_me').toggleClass('low-index');
     $('#projects').toggleClass('low-index');
   });
+
+$('.nav-list-item').on('click', function(){
+    $('.slide-in').toggleClass('show');
+    $('#about_me').toggleClass('low-index');
+    $('#projects').toggleClass('low-index');
+})
