8e4869ef719705399444180b76ecd4347db30683
Patrick Connelly <patrickconnelly95@gmail.com>
2018-05-14 12:11:54 -0500

    update aesthetics

--- assets/js/myother.js
+++ assets/js/myother.js
@@ -1,4 +1,8 @@
 // Menu
-$('#nav-menu').on('click', function(){
-    $('.nav-list-item-container').toggle();
-})
+// $('#nav-menu').on('click', function(){
+//     $('.nav-list-item-container').toggle();
+// })
+$('#nav-menu').on('click', function() {
+    console.log('clicked');
+    $('.slide-in').toggleClass('show');
+  });
